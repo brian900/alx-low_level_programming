84a6d6ed371a24201492fda3dc604d3f94c30df9
brian masakari <brianmasakari@gmail.com>
2023-02-16 13:59:36 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The programme prints various computer sizes
+ * Return:0 (success)
+ */
+int main(void)
+{
+	char a;
+	int f;
+	long int b;
+	long long int e;
+	float q;
+Printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+Printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(f));
+Printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+Printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+Printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
+return (0);
+}
