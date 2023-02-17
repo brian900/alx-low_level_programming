7e092a981bfaa7e7a958e21c7ac6486da4ca9831
brian masakari <brianmasakari@gmail.com>
2023-02-17 11:25:39 -0800

    if_else_variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - The program uses a if else statements to print a negaive.
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	int n;
@@ -10,5 +15,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is null\n", n);
+
+	else
+		printf("%d is negative\n", n);
 	return (0);
 }
