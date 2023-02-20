53f95c24c035e5ff91cfd078eaa94645a51fff7a
brian masakari <brianmasakari@gmail.com>
2023-02-20 11:29:38 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the programme uses putchar to print all letters except q&e
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			break;
+		}
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
