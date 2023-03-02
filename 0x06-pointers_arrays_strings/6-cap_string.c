a4843eacecdefcde4b909ff93d8ada8ada543423
brian masakari <brianmasakari@gmail.com>
2023-03-02 13:41:20 -0800

    ./6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,33 @@
 #include "main.h"
 /**
  * cap_string - function that capitalizes all words of a string
- * @s: string being capitalized
+ * @str: string being capitalized
  * Return: capitalized string
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int Capt = 0;
+	int index  = 0;
 
-	while (s[Capt])
+	while (str[index])
 	{
-		while (!(s[Capt] >= 'a' && s[Capt] <= 'z'))
-			Capt++;
-
-		if (s[Capt - 1] == ' ' ||
-				s[Capt - 1] == '\t' ||
-				s[Capt - 1] == '\n' ||
-				s[Capt - 1] == ',' ||
-				s[Capt - 1] == ';' ||
-				s[Capt - 1] == '.' ||
-				s[Capt - 1] == '!' ||
-				s[Capt - 1] == '?' ||
-				s[Capt - 1] == '"' ||
-				s[Capt - 1] == '(' ||
-				s[Capt - 1] == ')' ||
-				s[Capt - 1] == '{' ||
-				s[Capt - 1] == '}' ||
-				Capt == 0)
-			s[Capt] -= 32;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
+			str[index] -= 32;
 
 	}
-	return (s);
+	return (str);
 }
