43622a3063e755c3d23f5f13887cccbd7bece959
brian masakari <brianmasakari@gmail.com>
2023-03-15 12:52:01 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,17 +4,18 @@
 
 /**
  * free_grid - function that frees a 2 dimensional grid.
- * @height: height of the array
- * @grid: 2 dimensional array
+ * @height: height of the 2 dimensional grid
+ * @grid: 2 dimensional grid
  * Return: empty
  */
 
 void free_grid(int **grid, int height)
 {
-	for (int i = 0; i < height; i++)
+	int i;
+
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
-
 	free(grid);
 }
